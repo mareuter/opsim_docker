ac5920a38de545e28de81948dc0a2d85c865462f
Michael Reuter <mareuternh@gmail.com>
2016-01-29 10:40:21 -0700

    Latest tweaks.

--- opsim3/startup_nersc.sh
+++ opsim3/startup_nersc.sh
@@ -6,9 +6,10 @@ $HOME/opsim-config/etc/init.d/mysqld start
 
 cd $HOME/conf
 git checkout $CONFIG_SHA1
+STARTUP_COMMENT=$(git log -n 1 --pretty=format:%s)
 
 cd $HOME/runs
 
 source eups-setups.sh
 setup sims_operations
-opsim.py --track=no --config=$HOME/conf/survey/LSST.conf --startup_comment=$STARTUP_COMMENT >& log/opsim.log
+opsim.py --track=no --config=$HOME/conf/survey/LSST.conf --startup_comment="$STARTUP_COMMENT" >& log/opsim.log
