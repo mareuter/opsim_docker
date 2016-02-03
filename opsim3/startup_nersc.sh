2d906341c44c66b1d75ea04fd1dc564a95aa4b91
Michael Reuter <mareuternh@gmail.com>
2016-02-03 14:27:52 -0700

    Adding post-processing and file renaming.

--- opsim3/startup_nersc.sh
+++ opsim3/startup_nersc.sh
@@ -10,6 +10,16 @@ STARTUP_COMMENT=$(git log -n 1 --pretty=format:%s)
 
 cd $HOME/runs
 
+# Since container is fresh, sessionID will always be 1000
+oldfiletag="${HOSTNAME}_1000"
+newfiletag="${oldfiletag}_${CONFIG_SHA1}"
+
 source eups-setups.sh
 setup sims_operations
-opsim.py --track=no --config=$HOME/conf/survey/LSST.conf --startup_comment="$STARTUP_COMMENT" >& log/opsim.log
+opsim.py --track=no --config=$HOME/conf/survey/LSST.conf --startup_comment="$STARTUP_COMMENT" >& log/opsim_${newfiletag}.log
+mv log/lsst.log_1000 log/lsst.log_${newfiletag}
+
+$SIMS_OPERATIONS_DIR/tools/modifySchema.sh 1000 >& log/ms_${newfiletag}.log
+mv output/${oldfiletag}_datexport.tar.gz output/${newfiletag}_datexport.tar.gz
+mv output/${oldfiletag}_export.sql.gz output/${newfiletag}_export.sql.gz
+mv output/${oldfiletag}_sqlite.db output/${newfiletag}_sqlite.db
