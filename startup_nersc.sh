aee106b81e03a3592c9ad19a7dbf14fb4eda3968
Debbie Bard <djbard@gmail.com>
2016-02-11 14:50:25 -0800

    runs conf files from the scratch space

--- startup_nersc.sh
+++ startup_nersc.sh
@@ -9,6 +9,7 @@ setup sims_operations
 ### I'm mounting my scratch dir as /home/opsim/scratch/ which overwrites the existing dir.
 ### So I need to copy the contents back over. 
 cp -r /home/opsim/scratchtemp/* /home/opsim/scratch/
+cp -r /home/opsim/conftemp/* /home/opsim/scratch/conf
 
 ### Change the permissions (the image was mounted as read-only)
 ### This is a bit brute-force
@@ -19,9 +20,9 @@ chmod 777 /home/opsim/scratch/opsim-config/etc/init.d/mysqld
 /home/opsim/scratch/opsim-config/etc/init.d/mysqld start
 
 ## skip this step for now - will potentially want to move $HOME/conf to $HOME/scratch to allow write access
-##cd $HOME/conf
-##git checkout $CONFIG_SHA1
-##STARTUP_COMMENT=$(git log -n 1 --pretty=format:%s)
+#cd $HOME/conf
+#git checkout $CONFIG_SHA1
+#STARTUP_COMMENT=$(git log -n 1 --pretty=format:%s)
 
 
 cd /home/opsim/scratch/runs
@@ -32,7 +33,7 @@ mkdir output
 oldfiletag="${HOSTNAME}_1000"
 newfiletag="${oldfiletag}_${CONFIG_SHA1}"
 
-opsim.py --track=no --config=$HOME/conf/survey//LSST.conf --startup_comment="$STARTUP_COMMENT" >& $HOME/scratch/runs/log/opsim_${newfiletag}.log
+opsim.py --track=no --config=$HOME/scratch/conf/survey/LSST.conf --startup_comment="$STARTUP_COMMENT" >& $HOME/scratch/runs/log/opsim_${newfiletag}.log
 
 #mv $HOME/scratch/runs/log/lsst.log_1000 $HOME/scratch/runs/log/lsst.log_${newfiletag}
 
